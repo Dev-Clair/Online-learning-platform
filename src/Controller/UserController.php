2d2be810b38b0e02c6a0d88ea30d8b63d048f193
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-20 13:57:48 +0100

    feat: create register form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,6 +14,7 @@ class UserController extends AbstractController
         $user = [];
 
         return $this->render('user/index.html.twig', [
+            'title' => 'Users',
             'user' => $user,
         ]);
     }
