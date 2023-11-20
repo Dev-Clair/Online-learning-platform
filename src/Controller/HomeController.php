67a9d2499d690413ea925d07a2b4e0b392f46340
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-20 13:29:15 +0100

    feat: adding home controller|view

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+        $courses = [];
+        $instructors = [];
+        $testimonials = [];
+
+        return $this->render('home/index.html.twig', [
+            'courses' => $courses,
+            'instructors' => $instructors,
+            'testimonials' => $testimonials,
+        ]);
+    }
+}
