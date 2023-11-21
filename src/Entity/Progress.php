928d51b1b72d3a3b215e201e0842a1f97326733b
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-21 11:39:51 +0100

    feat: added progress entity

---
+++ src/Entity/Progress.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProgressRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProgressRepository::class)]
+class Progress
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $status = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $lastAccessed = null;
+
+    #[ORM\ManyToOne(inversedBy: 'progress')]
+    private ?Enrollment $enrollment = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getLastAccessed(): ?\DateTimeImmutable
+    {
+        return $this->lastAccessed;
+    }
+
+    public function setLastAccessed(\DateTimeImmutable $lastAccessed): static
+    {
+        $this->lastAccessed = $lastAccessed;
+
+        return $this;
+    }
+
+    public function getEnrollment(): ?Enrollment
+    {
+        return $this->enrollment;
+    }
+
+    public function setEnrollment(?Enrollment $enrollment): static
+    {
+        $this->enrollment = $enrollment;
+
+        return $this;
+    }
+}
