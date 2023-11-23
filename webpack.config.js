c58273e114158f714993be15b887993540c550cc
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-23 15:47:52 +0100

    fix: refactor client side

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,6 @@ Encore
    * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
    */
   .addEntry("app", "./assets/app.js")
-  .addEntry("app", "./assets/js/main.js")
 
   // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
   .splitEntryChunks()
