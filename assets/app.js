6831e403f593d60543ecca44f7dce128b19848eb
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-23 16:34:03 +0100

    fix: cofiguring webpack

--- assets/app.js
+++ assets/app.js
@@ -8,4 +8,44 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.css";
 
+import "./img/about.jpg";
+
+import "./img/carousel-1.jpg";
+
+import "./img/carousel-2.jpg";
+
+import "./img/cat-1.jpg";
+
+import "./img/cat-2.jpg";
+
+import "./img/cat-3.jpg";
+
+import "./img/cat-4.jpg";
+
+import "./img/course-1.jpg";
+
+import "./img/course-2.jpg";
+
+import "./img/course-3.jpg";
+
+import "./img/team-1.jpg";
+
+import "./img/team-2.jpg";
+
+import "./img/team-3.jpg";
+
+import "./img/team-4.jpg";
+
+import "./img/testimonial-1.jpg";
+
+import "./img/testimonial-2.jpg";
+
+import "./img/testimonial-3.jpg";
+
+import "./img/testimonial-4.jpg";
+
+import "./css/style.css";
+
+import "./scss/bootstrap.scss";
+
 import "./js/main";
