dd7bd529486932fa6b38b871dd3e481d3c0aebb4
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-11-24 10:49:42 +0100

    fix: user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    #[IsGranted('ROLE_INSTRUCTOR')]
+    public function index(UserRepository $userRepository): Response
     {
-        $user = [];
+        $user = $userRepository->findAll();
 
         return $this->render('user/index.html.twig', [
             'title' => 'Users',
