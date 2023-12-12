381f8610701694fbdfc42c99c315c0aa198ebaba
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-12 13:17:55 +0100

    fix: courses entity

--- src/Entity/Courses.php
+++ src/Entity/Courses.php
@@ -39,11 +39,15 @@ class Courses
     #[ORM\OneToMany(mappedBy: 'courses', targetEntity: Lesson::class, orphanRemoval: true)]
     private Collection $lessons;
 
+    #[ORM\OneToMany(mappedBy: 'courses', targetEntity: Testimonial::class)]
+    private Collection $testimonials;
+
     public function __construct()
     {
         $this->createdAt = new \DateTimeImmutable();
         $this->enrollments = new ArrayCollection();
         $this->lessons = new ArrayCollection();
+        $this->testimonials = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -179,6 +183,36 @@ class Courses
     public function setDuration(int $duration): static
     {
         $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Testimonial>
+     */
+    public function getTestimonials(): Collection
+    {
+        return $this->testimonials;
+    }
+
+    public function addTestimonial(Testimonial $testimonial): static
+    {
+        if (!$this->testimonials->contains($testimonial)) {
+            $this->testimonials->add($testimonial);
+            $testimonial->setCourses($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTestimonial(Testimonial $testimonial): static
+    {
+        if ($this->testimonials->removeElement($testimonial)) {
+            // set the owning side to null (unless already changed)
+            if ($testimonial->getCourses() === $this) {
+                $testimonial->setCourses(null);
+            }
+        }
 
         return $this;
     }
