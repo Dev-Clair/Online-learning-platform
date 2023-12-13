cdae9424e00d24b251cf8796809067e87de7b6c4
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-13 13:22:33 +0100

    fix: testimonial controller

--- src/Controller/TestimonialController.php
+++ src/Controller/TestimonialController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Testimonial;
+use App\Form\TestimonialType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,5 +21,24 @@ class TestimonialController extends AbstractController
             'title' => 'Testimonials',
             'testimonials' => $testimonials ?? "",
         ]);
+    }
+
+    #[Route('/review', name: 'app_testimonial_form')]
+    public function create(): Response
+    {
+        $testimonials = [];
+
+        return $this->render('testimonial/create.html.twig', ['title' => 'Reviews']);
+    }
+
+    #[Route('/review', name: 'app_testimonial_create')]
+    public function store(): Response
+    {
+        $testimonials = [];
+
+        // return $this->render('testimonial/index.html.twig', [
+        //     'title' => 'Testimonials',
+        //     'testimonials' => $testimonials ?? "",
+        // ]);
     }
 }
