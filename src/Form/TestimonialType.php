bff5ecd586432f320f98b6fa14a2c5e9cfa09c9a
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-18 14:47:22 +0100

    fix: testimonial form|controller|view

--- src/Form/TestimonialType.php
+++ src/Form/TestimonialType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Testimonial;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,9 @@ class TestimonialType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('testimonial');
+            ->add('name', TextType::class)
+            ->add('email', TextType::class)
+            ->add('testimonial', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
