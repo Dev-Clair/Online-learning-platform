6af5b682ebd8d15649b60c9cc1e4f1216f3680d9
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-18 15:48:17 +0100

    fix: contact

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,33 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $contact = [];
+        // Create new testimonial instance 
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact, ['method' => 'post', 'action' => 'app_contact']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Retrieve values from form fields
+            $form_name = ucwords($form->get('name')->getData());
+            $form_email = $form->get('email')->getData();
+            $form_subject = $form->get('subject')->getData();
+            $form_message = $form->get('message')->getData();
+
+            // Assign form values to entity properties
+            $contact->setName($form_name);
+            $contact->setEmail($form_email);
+            $contact->setSubject($form_subject);
+            $contact->setMessage($form_message);
+
+            // Create new contact
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_testimonial');
+        }
 
         return $this->render('contact/index.html.twig', [
             'title' => 'Contact',
-            'contact' => $contact ?? "",
+            'contactForm' => $form->createView()
         ]);
     }
-
-    // #[Route('/contact', name: 'app_contact')]
-    // public function store(): Response
-    // {
-    //     $inquiry_error = false;
-
-    //     $inquiry_success = true;
-
-    //     // return $this->render('contact/index.html.twig', [
-    //     //     'title' => 'Contact',
-    //     //     'inquiry_response' => $inquiry_success ?? $inquiry_error
-    //     // ]);
-    // }
 }
