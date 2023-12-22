de81a983da775781738e8014f48a796a62307d18
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-22 19:31:49 +0100

    fix: modified accessor method

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -29,7 +29,7 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            $user->setRoles(["ROLE_USER"]);
+            $user->setRoles(["ROLE_STUDENT"]);
 
             $entityManager->persist($user);
             $entityManager->flush();
