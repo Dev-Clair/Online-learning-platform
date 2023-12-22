6fd87bd5d13c4bca0552499c38049aa17509049d
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-22 18:03:22 +0100

    fix: home controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,15 +11,6 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        $courses = [];
-        $instructors = [];
-        $testimonials = [];
-
-        return $this->render('home/index.html.twig', [
-            'title' => 'Home',
-            'courses' => $courses ?? "",
-            'instructors' => $instructors ?? "",
-            'testimonials' => $testimonials ?? "",
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
