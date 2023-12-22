c8789e2b60b547b6890f7e6d55e0f12660e9fa15
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-22 18:04:16 +0100

    fix: profiletype

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -2,8 +2,12 @@
 
 namespace App\Form;
 
+use AddressInfo;
 use App\Entity\Profile;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +16,10 @@ class ProfileType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('dateOfBirth')
-            ->add('address')
-            ->add('country')
-            ->add('user');
+            ->add('dateOfBirth', DateType::class)
+            ->add('address', TextType::class)
+            ->add('country', CountryType::class)
+            ->add('user', UserType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
