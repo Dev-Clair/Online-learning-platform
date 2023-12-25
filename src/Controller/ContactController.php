6dfefd8f197bf735e10340d3cf3dc46bd669a283
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-25 19:47:50 +0100

    fix: contact entity|view

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,15 +4,18 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactFormType;
+use App\Repository\ContactRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[Route('/contact')]
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'app_contact', methods: ['GET', 'POST'])]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         // Create new contact instance 
@@ -34,9 +37,29 @@ class ContactController extends AbstractController
             return $this->redirectToRoute('app_contact');
         }
 
-        return $this->render('contact/index.html.twig', [
-            'title' => 'Contact',
-            'contactForm' => $form->createView()
+        return $this->render('contact/index.html.twig', ['contactForm' => $form]);
+    }
+
+    #[Route('/show', name: 'app_contact_show', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_CUSTOMER_CARE')]
+    public function show(ContactRepository $contactRepository): Response
+    {
+        return $this->render('contact/show.html.twig', [
+            'contact' => $contactRepository->findAll()
         ]);
+    }
+
+    #[Route('/care/{id}/edit', name: 'app_contact_care', methods: ['POST'])]
+    #[IsGranted('ROLE_CUSTOMER_CARE')]
+    public function customercare(Contact $contact, EntityManagerInterface $entityManager): Response
+    {
+        // Set status to 1 (Reviewed)
+        $contact->setStatus(1);
+
+        // Update status in database
+        $entityManager->persist($contact);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_contact_show');
     }
 }
