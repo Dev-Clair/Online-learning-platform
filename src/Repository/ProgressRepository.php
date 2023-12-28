36632c69d3f79a16cd61707f32581233eb1a2961
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-28 21:37:06 +0100

    fix: removed comment block

--- src/Repository/ProgressRepository.php
+++ src/Repository/ProgressRepository.php
@@ -20,29 +20,4 @@ class ProgressRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Progress::class);
     }
-
-//    /**
-//     * @return Progress[] Returns an array of Progress objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('p.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Progress
-//    {
-//        return $this->createQueryBuilder('p')
-//            ->andWhere('p.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
