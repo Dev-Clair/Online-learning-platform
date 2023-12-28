72adc5d445c084abb6acbde0ef7bd6729306152b
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-28 11:38:45 +0100

    fix: removed commented blocks

--- src/Repository/EnrollmentRepository.php
+++ src/Repository/EnrollmentRepository.php
@@ -20,29 +20,4 @@ class EnrollmentRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Enrollment::class);
     }
-
-//    /**
-//     * @return Enrollment[] Returns an array of Enrollment objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('e.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Enrollment
-//    {
-//        return $this->createQueryBuilder('e')
-//            ->andWhere('e.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
