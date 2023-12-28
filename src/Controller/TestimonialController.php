8228491d7415fd4b9e04c195bf662a795774d2ad
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2023-12-28 21:59:27 +0100

    fix: testimonial crud

--- src/Controller/TestimonialController.php
+++ src/Controller/TestimonialController.php
@@ -10,12 +10,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class TestimonialController extends AbstractController
 {
     #[Route('/testimonial', name: 'app_testimonial', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_STUDENT')]
     public function index(Request $request, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
     {
         // Create new testimonial instance 
@@ -24,22 +22,14 @@ class TestimonialController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-
-            // Retrieve the email submitted by user
-            $form_email = $form->get('email')->getData();
-            // Check if a valid user with the email address exists in the database
-            $user = $userRepository->findOneBy(['email' => $form_email]);
-
-            if ($user) {
-
-                // Create new testimonial
-                $entityManager->persist($testimonial);
-                $entityManager->flush();
-
-                $this->addFlash('success', 'Thanks for helping us get better! Kindly subscribe to our newsletter to be in the loop about our future events and activities.');
-
+            // Verify Permissions
+            if (!$this->isGranted('ROLE_STUDENT')) {
                 return $this->redirectToRoute('app_testimonial');
             }
+
+            // Create new testimonial
+            $entityManager->persist($testimonial);
+            $entityManager->flush();
 
             return $this->redirectToRoute('app_testimonial');
         }
@@ -48,8 +38,7 @@ class TestimonialController extends AbstractController
         $testimonials = $entityManager->getRepository(Testimonial::class)->findAll() ?? [];
 
         return $this->render('testimonial/index.html.twig', [
-            'title' => 'Testimonial',
-            'testimonials' => $testimonials ?? "",
+            'testimonials' => $testimonials ?? [],
             'testimonialForm' => $form
         ]);
     }
