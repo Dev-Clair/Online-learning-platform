96322f97c2dc735534fee805cf1986272d6675b8
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-01-04 20:15:36 +0100

    fix: implement caching

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,21 +5,26 @@ namespace App\Controller;
 use App\Repository\CoursesRepository;
 use App\Repository\TestimonialRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(CoursesRepository $coursesRepository, TestimonialRepository $testimonialRepository): Response
+    public function index(CoursesRepository $coursesRepository, TestimonialRepository $testimonialRepository, CacheInterface $fileSystemCache): Response
     {
-        $courses = $coursesRepository->findAll() ?? [];
+        $homeController = $fileSystemCache->get('app_home', function (ItemInterface $item) use ($coursesRepository, $testimonialRepository): array {
+            $item->expiresAfter(1800);
 
-        $testimonials = $testimonialRepository->findAll() ?? [];
+            return [
+                'courses' => $coursesRepository->findBy([], [], limit: 2, offset: 0) ?? [],
 
-        return $this->render('home/index.html.twig', [
-            'courses' => $courses,
-            'testimonials' => $testimonials
-        ]);
+                'testimonials' => $testimonialRepository->findBy([], [], limit: 2, offset: 0) ?? []
+            ];
+        });
+
+        return $this->render('home/index.html.twig', $homeController);
     }
 }
