2a0afc1bc132355cc20a17358535a6fe51f852ef
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-01-05 09:13:16 +0100

    fix: team controller|view

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class TeamController extends AbstractController
 {
     #[Route('/team', name: 'app_team')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
-        return $this->render('team/index.html.twig');
+        return $this->render('team/index.html.twig', [
+            'instructors' => $userRepository->getInstructors() ?? []
+        ]);
     }
 }
