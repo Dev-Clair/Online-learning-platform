7216407c2f1fcde11df5e0125d73adcc2ddcd218
Aniogbu Samuel <aniogbu.samuel@yahoo.com>
2024-01-09 10:57:45 +0100

    fix: chapter route mapping

--- src/Form/ChapterType.php
+++ src/Form/ChapterType.php
@@ -15,16 +15,16 @@ class ChapterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('courses', EntityType::class, [
-                'mapped' => true,
-                'class' => Courses::class,
-                'choice_label' => function (Courses $courses) {
-                    return sprintf('%s', $courses->getTitle());
-                },
-                'placeholder' => '-- Select Course --',
-                'attr' => ['class' => 'form-select']
-            ]);
+            ->add('title');
+        // ->add('courses', EntityType::class, [
+        //     'mapped' => true,
+        //     'class' => Courses::class,
+        //     'choice_label' => function (Courses $courses) {
+        //         return sprintf('%s', $courses->getTitle());
+        //     },
+        //     'placeholder' => '-- Select Course --',
+        //     'attr' => ['class' => 'form-select']
+        // ]);
         // ->add('user', EntityType::class, [
         //     'mapped' => true,
         //     'class' => User::class,
